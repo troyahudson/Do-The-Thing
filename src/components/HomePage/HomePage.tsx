ff3f5381b0382dc9c8862c52bd764cccebce1f7a
troyahudson <94701592+troyahudson@users.noreply.github.com>
2022-03-15 11:29:44 -0400

    updated landing page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -33,13 +33,13 @@ export default function HomePage({ }: Props) {
       <div className='welcome-message-root'>
         <img className='logo' src="https://i.postimg.cc/gkF2VzXN/dott-logo-text-green.png" />
         <p><b>Do The Thing</b>&trade; is a project management app for people who hate project management apps.</p>
-        <p>Right now, it can help you manage and organize personal to-do lists and tasks. DoTT<sup>*</sup> is still in active development,
-          and we're working on growing it into a genuinely useful project management app.</p>
         <p>To get started, <Link to="/signup">
           sign up for a free account
         </Link>, <br />
           or <Link to="/login">
             login</Link> if you're already signed up.</p>
+        <p>DoTT<sup>*</sup> is still in active development,
+          and we're working on growing it into a genuinely useful project management app.</p>
         <p>For more information about DoTT and my other projects, check out my website,
           <br />
           <a href="http://troyahudson.com" target="_blank" rel="noopener noreferrer">
@@ -47,7 +47,7 @@ export default function HomePage({ }: Props) {
           </a>.
         </p>
         <hr />
-        <div className='footnote'><sup>*</sup>Yep, we call it DoTT. Feel free to come up with your own nickname or term of endearment, but we're sticking with DoTT.</div>
+        <div className='footnote'><sup>*</sup>Yep, we call it DoTT. Feel free to come up with your own nickname or term of endearment!</div>
       </div>
     )
 
