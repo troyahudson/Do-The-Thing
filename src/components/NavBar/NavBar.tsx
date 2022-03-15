30548cab44f4cbe3014086fd54e7a1a2f742e46b
troyahudson <94701592+troyahudson@users.noreply.github.com>
2022-03-15 11:31:42 -0400

    added breadcrumb navigation

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,9 @@
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBuilding, faUser } from '@fortawesome/free-regular-svg-icons';
+import { faBuildingColumns, faHouseUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { stringify } from 'querystring';
-import React, { useContext, useEffect } from 'react'
-import { Link, Outlet, useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { Context } from '../../App'
 import './NavBar.css'
 
@@ -12,8 +13,11 @@ export default function NavBar({ }: Props) {
 
     const { activeUser, setActiveUser, activeProject, setActiveProject, localStorage } = useContext(Context);
     var user = localStorage.getActiveUser();
-    // var project = localStorage.getActiveProject();
+    // const [project, setProject] = useState(localStorage.getActiveProject());
     const navigate = useNavigate();
+    const location = useLocation();
+    const isProjectPage = location.pathname.split("/")[1] === "project";
+    const isUserPage = location.pathname.split("/")[1] === "user";
 
     function logOutUser() {
         navigate("/");
@@ -27,19 +31,41 @@ export default function NavBar({ }: Props) {
         user = localStorage.getActiveUser();
     }, [activeUser?.id]);
 
+    useEffect(() => {
+
+
+        if (isProjectPage) {
+            setActiveProject(localStorage.getActiveProject());
+        } else {
+            setActiveProject(null)
+            localStorage.removeActiveProject();
+        }
+
+    }, [location]);
+
+    console.log(activeProject?.name);
+
+
     return (
-        <div>
-        <div className='nav-bar-root'>
-            <div>
-                <Link to={user ? `/user/${user?.id}` : '/'}>
-                    <img className='logo' src="https://i.postimg.cc/qvh7dN3c/dott-logo-white.png" />
-                </Link>
+        <>
+            <div className='nav-bar-root'>
+                <div>
+                    <Link to={user ? `/workspace/${user?.id}` : '/'}>
+                        <img className='logo' src="https://i.postimg.cc/qvh7dN3c/dott-logo-white.png" />
+                    </Link>
+                </div>
+                <div className='nav-items'>
+                    {!user && <Link to="/login">Login</Link>}
+                    {user && <div onClick={logOutUser}>Log Out</div>}
+                </div>
             </div>
-            <div className='nav-items'>
-                {!user && <Link to="/login">Login</Link>}
-                {user && <div onClick={logOutUser}>Log Out</div>}
-            </div>
-        </div>
-        </div>
+            {user && <ul className="breadcrumb">
+                <li><Link to={`/user/${user?.id}`}><FontAwesomeIcon icon={faHouseUser} /> {user?.email}</Link></li>
+                {!isUserPage && <li><Link to={`/workspace/${user?.id}`}>{user?.orgName}</Link></li>}
+                {activeProject && <li><a href="#">{activeProject?.name}</a></li>}
+                {/* <li><a href="#"></a></li> */}
+                {/* <li>Task Name</li> */}
+            </ul>}
+        </>
     )
 }
